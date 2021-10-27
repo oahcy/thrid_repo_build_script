d2592de05c1e78dbd6056b45da6a20cb95aab466
yangchao <1162485779@qq.com>
2021-10-27 10:58:58 +0800

    add cmd record

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ function RunCommand(cmd, ...cmds) {
   return null;
 }
 
+function MakeDir(path) {
+  if (!fs.existsSync(path)) fs.mkdirSync(path,{recursive: true});
+  command_recorder.AddCommand("mkdir %s", path);
+}
+
 function ReplaceScriptTmp(from_lists, to_lists) {
   const replace = require('replace-in-file');
   const options = {
@@ -36,6 +41,38 @@ function ReplaceScriptTmp(from_lists, to_lists) {
     console.log(result);
   } catch(err) {
     console.log('error:', err);
+  }
+}
+
+function CommandRecord() {
+  this.commands = [];
+  this.AddCommand = (cmd, ...cmds) => {
+  const command = util.format(cmd, ...cmds);
+    this.commands.push(command);
+    return this;
+  }
+  this.AddCdCmd = (path) => {
+    const cmd = "cd " + path;
+    this.commands.push(cmd);
+    return this;
+  }
+  this.AddEnvCmd = (env_map) => {
+    for (var index in env_map) {
+      const cmd = util.format("set %s=%s", index, env_map[index]);
+      this.commands.push(cmd);
+    }
+    return this;
+  }
+  this.SaveCmd = (file_name) => {
+    fs.writeFileSync(file_name, this.commands.join('\n'));
+    return this;
+  }
+  this.AddNote = (note) => {
+    this.commands.push(util.format("echo %s", note));
+    return this;
+  }
+  this.PassBy = () => {
+    return true;
   }
 }
 
@@ -67,6 +104,7 @@ function PathOpt() {
     var current_path = process.cwd();
     if (fs.existsSync(dst_path)) {
       process.chdir(dst_path);
+      command_recorder.AddCdCmd(dst_path);
       console.log("enter:" + dst_path);
       this.current_path = dst_path;
       this.store_paths.push(current_path);
@@ -78,12 +116,14 @@ function PathOpt() {
       console.log("leave:" + this.current_path);
       console.log("current:" + p);
       process.chdir(p);
+      command_recorder.AddCdCmd(p);
     }
   }
 };
 
 var path_opt = new PathOpt();
 var user_envs = new Env();
+var command_recorder = new CommandRecord();
 
 console.log(shell.exec('git --version'));
 
@@ -95,7 +135,7 @@ function RunTask() {
 }
 
 function UnExtWithSuitableFormat(file, out_dir) {
-  if (!fs.existsSync(out_dir)) fs.mkdirSync(out_dir,{recursive: true});
+  MakeDir(out_dir)
   const ext_name = path.extname(file).toLowerCase();
   if (ext_name == ".zip") {
     RunCommand("unzip -o %s -d %s", file, out_dir);
@@ -108,7 +148,9 @@ function UnExtWithSuitableFormat(file, out_dir) {
 
 function BuildByCOnfigure(bld_path, configure_path, config) {
   path_opt.Push(bld_path);
-  RunCommand("%s --prefix=%s %s", configure_path, config.output_path, configure_shared_libs);
+  if (!command_recorder.AddCommand("%s --prefix=%s %s", configure_path, config.output_path, configure_shared_libs).PassBy()) {
+    RunCommand("%s --prefix=%s %s", configure_path, config.output_path, configure_shared_libs);
+  }
 
   if (config.after_build) {
     path_opt.Push(bld_path);
@@ -116,8 +158,12 @@ function BuildByCOnfigure(bld_path, configure_path, config) {
     RunCommand(shell_path);
     path_opt.Pop();
   }
-  RunCommand("make");
-  RunCommand("make install");
+  if (!command_recorder.AddCommand("make").PassBy()) {
+    RunCommand("make");
+  }
+  if (!command_recorder.AddCommand("make install").PassBy()) {
+    RunCommand("make install");
+  }
   path_opt.Pop();
 }
 
@@ -134,10 +180,11 @@ function Build(bld_path, config) {
     const env_path_str = fs.readFileSync(env_path).toString();
     const env_obj = JSON.parse(env_path_str);
     user_envs.AddEnv(env_obj);
+    command_recorder.AddEnvCmd(env_obj);
   }
   //
   const dst_bld_path = bld_path;
-  if (!fs.existsSync(dst_bld_path)) fs.mkdirSync(dst_bld_path,{recursive: true});
+  MakeDir(dst_bld_path);
   if (config.build_type == "configure") {
     if (config.use_clang) {
       user_envs.AddEnv({"CC": "/usr/bin/clang"});
@@ -153,10 +200,19 @@ function Build(bld_path, config) {
     if (config.use_clang) {
       cmake_params = cmake_params + " -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ "
     }
-    RunCommand("cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -S%s -B%s -DCMAKE_INSTALL_PREFIX:PATH=%s %s %s", config.repe_path, dst_bld_path, config.output_path,cmake_shared_libs, cmake_params);
+    var cmd = util.format("cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -S%s -B%s -DCMAKE_INSTALL_PREFIX:PATH=%s %s %s", config.repe_path, dst_bld_path, config.output_path,cmake_shared_libs, cmake_params);
+    if (!command_recorder.AddCommand(cmd).PassBy()) {
+      RunCommand(cmd);
+    }
     path_opt.Push(dst_bld_path);
-    RunCommand("make");
-    RunCommand("make install");
+    cmd = "make";
+    if (!command_recorder.AddCommand(cmd).PassBy()) {
+      RunCommand(cmd);
+    }
+    cmd = "make install";
+    if (!command_recorder.AddCommand(cmd).PassBy()) {
+      RunCommand(cmd);
+    }
     path_opt.Pop();
   }
 }
@@ -178,15 +234,15 @@ function RunByConfig(file_path) {
   //replace script tmp
   ReplaceScriptTmp([/\{OUTPUT_PATH\}/g], output_path);
 
-  if (!fs.existsSync(dst_path)) fs.mkdirSync(dst_path,{recursive: true});
-  if (!fs.existsSync(downlaod_dst_path)) fs.mkdirSync(downlaod_dst_path,{recursive: true});
-  if (!fs.existsSync(output_path)) fs.mkdirSync(output_path,{recursive: true});
+  MakeDir(dst_path);
+  MakeDir(downlaod_dst_path);
+  MakeDir(output_path);
 
   path_opt.Push(dst_path);
 
   config_obj.packages.forEach((v) => {
     console.log(v);
-
+    command_recorder.AddNote(v.name + " start ...");
     v.output_path = path.join(output_path, v.name);
     v.work_path = work_path;
     v.bld_path = path.join(bld_dst_path, v.name);
@@ -194,7 +250,7 @@ function RunByConfig(file_path) {
       v.use_clang = config_obj.use_clang;
     }
 
-    if (fs.existsSync(v.output_path)) {
+    if (fs.existsSync(v.output_path) && !command_recorder.PassBy()) {
       console.log(util.format("pass : %s, you can delete this dir for recompile.", v.output_path));
       return;
     }
@@ -223,7 +279,9 @@ function RunByConfig(file_path) {
 
       Build(v.bld_path, v);
     }
+    command_recorder.AddNote(v.name + " stop");
   });
+  command_recorder.SaveCmd("cmd.sh");
 }
 
 RunByConfig("custom_config.json");
